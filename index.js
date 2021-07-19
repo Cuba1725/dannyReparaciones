99c7350021cb1ad2b8588c3cf4aeca981a8fa30f
Cuba1725 <danielmedina89@gmail.com>
2021-07-18 21:04:39 -0300

    todavia no se para donde ir

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
 
+let contactInfo = firebase.database().ref("infos");
+document.querySelector(".contacto").addEventListener("submit", submitform);
+ 
+    function submitform(e) {
+        e.preventDefault();
+
+    let nombre = document.querySelector(".nombre").value;
+    let correo = document.querySelector(".correo").value;
+    let mensaje = document.querySelector(".mensaje").value;
+    
+
+    saveContactInfo(nombre, correo, mensaje);
+    document.querySelector(".contacto").reset();
+
+    if(nombre === '' || correo === '' || mensaje === ''){
+        Swal.fire({
+            title: "Error",
+            text: "Todos los campos son obligatorios",
+            icon: "error",
+            confirmButtonText: "Confirmar"
+        })
+    } else {
+        Swal.fire({
+            title: `${nombre}`,
+            text: "Tu mensaje fue enviado con exito!",
+            icon: "success",
+            confirmButtonText: "Confirmar"
+        })
+    }
+    }
+    
+
+
+function saveContactInfo(nombre, correo, mensaje) {
+    let nuevoContacto = contactInfo.push();
+
+    nuevoContacto.set({
+        nombre: nombre,
+        correo: correo,
+        mensaje: mensaje,
+    });
+}
+
 
 
